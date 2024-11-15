ceffcb27e8539b66c8989c664629209e270cc7f4
aliviahanes <livysurface@gmail.com>
2024-11-14 19:03:39 -0600

    Update pet.py

--- pet.py
+++ pet.py
@@ -1,70 +1,21 @@
-from pets import Pet
-import random
-
-def stats(my_pet):
-  print(f'Happiness: {my_pet.happy}')
-  print(f'Hunger: {my_pet.hunger}')
-  print(f'Health: {my_pet.health}')
-
-def stats_deplete(my_pet):
-   my_pet.health -= random.randint(5,10)
-   my_pet.hunger -= random.randint(5,10)
-   my_pet.happy -= random.randint(5,10)
-
-def menu():
-  print('1) Pet')
-  print('2) Walk')
-  print('3) Feed')
-  print('4) Cage')
-  print('5) Quit')
-  user_choice = int(input('What would you like to do?: '))
-  return user_choice
 
 
-def main():
-  new_pet = Pet(input('Give your new pet a name. '))
-  print(f'This is your new pet, {new_pet.name}.\n')
-  choice = 0
-  while choice != 5 and new_pet.health > 0 and new_pet.hunger > 0 and new_pet.happy > 0:
-    stats(new_pet)
-    choice = menu()
-    print('\n')
-    stats_deplete(new_pet)
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.happy = 100
+        self.hunger = 100
+        self.health = 100
 
-    if choice == 1:
-      new_pet.pet()
-      print(f'You pet {new_pet.name}.\n')
+    def feed(self):
+      self.hunger += 20
 
+    def walk(self):
+      self.health += 10
+      self.happy += 5
 
+    def pet(self):
+      self.happy += 10
 
-    if choice == 2:
-      new_pet.walk()
-      print(f'You took {new_pet.name} on a walk!\n')
-
-    if choice == 3:
-      new_pet.feed()
-      print(f'You fed {new_pet.name}.\n')
-
-    if choice == 4:
-      new_pet.cage()
-      print(f"You put {new_pet.name} in it's cage :(\n")
-
-  if new_pet.health <= 0 or new_pet.hunger < 0 or new_pet.happy <= 0:
-    print(f'{new_pet.name} died :( .\n')
-  
-  else:
-    print('Come back later!')
-
-main()
-
-
-
-    
-
-
-
-    
-
-
-
-
+    def cage(self):
+      self.happy -= 10
